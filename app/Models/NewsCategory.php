692ad2fb31a17afbe9612af9cae9e7ae0d0420f6
taposh99 <taposhmd2@gmail.com>
2024-07-30 13:24:35 +0600

    update

--- app/Models/NewsCategory.php
+++ app/Models/NewsCategory.php
@@ -4,10 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 
 class NewsCategory extends Model
 {
     use HasFactory;
     protected $guarded = ['id'];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+    
+    public function news(): BelongsTo
+    {
+        return $this->belongsTo(News::class);
+    }
 
 }
